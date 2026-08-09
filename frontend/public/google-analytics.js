173864fd8b2a763af746dc7165f63e405363297e
Bailey-Builds <lbailey@sekki.io>
2026-08-09 10:23:13 -0400

    Update Google Analytics measurement ID

--- frontend/public/google-analytics.js
+++ frontend/public/google-analytics.js
@@ -1,4 +1,4 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){window.dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-54Y4NXDS4R');
+gtag('config', 'G-7BXZ1NCBS5');
